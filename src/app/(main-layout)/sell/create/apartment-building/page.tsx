a8e66308043ce6dcc0e819617f68804fddb40404
Pushan T <pushan.sahanaganesh@gmail.com>
2025-06-22 17:03:23 +0530

    Add Prettier configuration and new apartment building schema with related files

--- src/app/(main-layout)/sell/create/apartment-building/page.tsx
+++ src/app/(main-layout)/sell/create/apartment-building/page.tsx
@@ -1,8 +1,8 @@
 export default function ApartmentBuildingPage() {
-    return (
-        <div>
-            <h1>Apartment Building Page</h1>
-            <p>This is the page for creating an apartment building listing.</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Apartment Building Page</h1>
+      <p>This is the page for creating an apartment building listing.</p>
+    </div>
+  )
 }
